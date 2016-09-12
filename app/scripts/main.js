26559b0a881623140c9577bb15b019e9993399d7
Mike Zevitas <mzevitas@gmail.com>
2016-09-11 21:28:40 -0400

    main layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('JavaScript!!!');
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+  $('.carousel').carousel({
+    interval: 3000
+  })  
+
+   $('.carousel').carousel('cycle')
